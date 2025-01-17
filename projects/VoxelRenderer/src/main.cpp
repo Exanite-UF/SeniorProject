5e2f2df4555570b38cc0db60120d31348fc64590
William Chen <exanite78012@gmail.com>
2025-01-17 16:02:17 -0500

    Link GLFW and create window

--- projects/VoxelRenderer/src/main.cpp
+++ projects/VoxelRenderer/src/main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include "GLFW/glfw3.h"
 
 int main() {
     std::cout << "Hello world" << std::endl;
+
+    if (!glfwInit())
+    {
+        throw std::runtime_error("Failed to initialize GLFW");
+    }
+
+    auto window = glfwCreateWindow(800, 600, "Voxel Renderer", nullptr, nullptr);
+    if (!window)
+    {
+        throw std::runtime_error("Failed to create window");
+    }
+
+    while (true)
+    {
+
+    }
 
     return 0;
 }
