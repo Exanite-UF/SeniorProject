6453b8782ef431207ad427f95f584f10bec8309d
William Chen <exanite78012@gmail.com>
2025-01-26 14:00:50 -0500

    Cleanup initialization code

--- projects/VoxelRenderer/src/main.cpp
+++ projects/VoxelRenderer/src/main.cpp
@@ -1,37 +1,45 @@
-#include <iostream>
+// Include GLEW before OpenGL and GLFW
 #include <GL/glew.h>
+
+#include <iostream>
 #include <GLFW/glfw3.h>
 #include <glm/common.hpp>
 #include <glm/vec4.hpp>
 
 int main() {
-    std::cout << "Hello world" << std::endl;
+    std::cout << "Starting Voxel Renderer" << std::endl;
 
+    // Init GLFW
     if (!glfwInit())
     {
         throw std::runtime_error("Failed to initialize GLFW");
     }
 
-    if (!glewInit())
-    {
-        throw std::runtime_error("Failed to initialize GLEW");
-    }
-
+    // Create window
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     auto window = glfwCreateWindow(800, 600, "Voxel Renderer", nullptr, nullptr);
-    if (!window)
+    if (window == nullptr)
     {
         throw std::runtime_error("Failed to create window");
     }
 
+    // Init GLEW
     glfwMakeContextCurrent(window);
+    glewExperimental = true;
+    if (glewInit() != GLEW_OK)
+    {
+        throw std::runtime_error("Failed to initialize GLEW");
+    }
 
-    glm::vec4 clearColor{1, 0, 0, 1};
-
+    // Main render loop
+    glm::vec4 clearColor{1, 0, 0, 1}; // TODO: Probably remove this vec4 usage, this is just to test GLM
+    glClearColor(clearColor.r, clearColor.g, clearColor.b, clearColor.a);
     while (!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
 
-        glClearColor(clearColor.r, clearColor.g, clearColor.b, clearColor.a);
         glClear(GL_COLOR_BUFFER_BIT);
 
         glfwSwapBuffers(window);
