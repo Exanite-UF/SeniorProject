655a7df1d9d826a841eeb865b7853f48eb2d3c0d
William Chen <exanite78012@gmail.com>
2025-02-02 17:04:34 -0500

    Move InputManager function implementations to .cpp file

--- projects/VoxelRenderer/src/InputManager.h
+++ projects/VoxelRenderer/src/InputManager.h
@@ -12,15 +12,8 @@ struct InputState
     glm::vec2 mousePosition {};
     glm::vec2 mouseScroll {};
 
-    bool isKeyHeld(const int key) const
-    {
-        return heldKeys.contains(key);
-    }
-
-    bool isButtonHeld(const int button) const
-    {
-        return heldButtons.contains(button);
-    }
+    bool isKeyHeld(int key) const;
+    bool isButtonHeld(int button) const;
 };
 
 struct Input
@@ -28,50 +21,17 @@ struct Input
     InputState current {};
     InputState previous {};
 
-    glm::vec2 getMousePosition() const
-    {
-        return current.mousePosition;
-    }
+    glm::vec2 getMousePosition() const;
+    glm::vec2 getMouseDelta() const;
+    glm::vec2 getMouseScroll() const;
 
-    glm::vec2 getMouseDelta() const
-    {
-        return current.mousePosition - previous.mousePosition;
-    }
+    bool isKeyHeld(int key) const;
+    bool isKeyPressed(int key) const;
+    bool isKeyReleased(int key) const;
 
-    glm::vec2 getMouseScroll() const
-    {
-        return current.mouseScroll;
-    }
-
-    bool isKeyHeld(const int key) const
-    {
-        return current.isKeyHeld(key);
-    }
-
-    bool isKeyPressed(const int key) const
-    {
-        return current.isKeyHeld(key) && !previous.isKeyHeld(key);
-    }
-
-    bool isKeyReleased(const int key) const
-    {
-        return !current.isKeyHeld(key) && previous.isKeyHeld(key);
-    }
-
-    bool isButtonHeld(const int button) const
-    {
-        return current.isButtonHeld(button);
-    }
-
-    bool isButtonPressed(const int button) const
-    {
-        return current.isButtonHeld(button) && !previous.isButtonHeld(button);
-    }
-
-    bool isButtonReleased(const int button) const
-    {
-        return !current.isButtonHeld(button) && previous.isButtonHeld(button);
-    }
+    bool isButtonHeld(int button) const;
+    bool isButtonPressed(int button) const;
+    bool isButtonReleased(int button) const;
 };
 
 class InputManager
@@ -82,49 +42,10 @@ private:
 public:
     Input input {};
 
-    void update()
-    {
-        input.previous = input.current;
-        input.current = next;
+    void update();
 
-        next.mouseScroll = glm::vec2(0);
-    }
-
-    void onKey(GLFWwindow* window, int key, int scancode, int action, int mods)
-    {
-        if (action == GLFW_PRESS)
-        {
-            next.heldKeys.insert(key);
-        }
-
-        if (action == GLFW_RELEASE)
-        {
-            next.heldKeys.erase(key);
-        }
-    }
-
-    void onMouseButton(GLFWwindow* window, int button, int action, int mods)
-    {
-        if (action == GLFW_PRESS)
-        {
-            next.heldButtons.insert(button);
-        }
-
-        if (action == GLFW_RELEASE)
-        {
-            next.heldButtons.erase(button);
-        }
-    }
-
-    void onCursorPos(GLFWwindow* window, double xpos, double ypos)
-    {
-        next.mousePosition.x = xpos;
-        next.mousePosition.y = ypos;
-    }
-
-    void onScroll(GLFWwindow* window, double xoffset, double yoffset)
-    {
-        next.mouseScroll.x = xoffset;
-        next.mouseScroll.y = yoffset;
-    }
+    void onKey(GLFWwindow* window, int key, int scancode, int action, int mods);
+    void onMouseButton(GLFWwindow* window, int button, int action, int mods);
+    void onCursorPos(GLFWwindow* window, double xpos, double ypos);
+    void onScroll(GLFWwindow* window, double xoffset, double yoffset);
 };
