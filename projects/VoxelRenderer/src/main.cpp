1d65815cf5e9a2cf7877765ee468444c636bf7a7
William Chen <exanite78012@gmail.com>
2025-02-23 01:40:01 -0500

    Improve crash logging and add todo for Windows

--- projects/VoxelRenderer/src/main.cpp
+++ projects/VoxelRenderer/src/main.cpp
@@ -3,8 +3,10 @@
 
 #include <GLFW/glfw3.h>
 
+#include <iostream>
 #include <src/Program.h>
 #include <src/utilities/Log.h>
+#include <stdexcept>
 
 int main()
 {
@@ -15,7 +17,14 @@ int main()
     }
     catch (const std::runtime_error& e)
     {
+        // TODO: No idea why this prints gibberish on Windows
         Log::log("Program crashed: Runtime error: " + std::string(e.what()));
+
+        return 1;
+    }
+    catch (...)
+    {
+        Log::log("Program crashed: Unknown error");
 
         return 1;
     }
