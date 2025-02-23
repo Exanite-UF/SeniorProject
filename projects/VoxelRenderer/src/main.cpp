1ff7a8e85ca3229de18cf1e28aee87ad1cba4efb
William Chen <exanite78012@gmail.com>
2025-02-23 01:21:01 -0500

    Add crash logging
    This should fix the issue where errors aren't getting reported when using the Visual Studio compiler

--- projects/VoxelRenderer/src/main.cpp
+++ projects/VoxelRenderer/src/main.cpp
@@ -4,11 +4,21 @@
 #include <GLFW/glfw3.h>
 
 #include <src/Program.h>
+#include <src/utilities/Log.h>
 
 int main()
 {
-    Program program;
-    program.run();
+    try
+    {
+        Program program;
+        program.run();
+    }
+    catch (const std::runtime_error& e)
+    {
+        Log::log("Program crashed: Runtime error: " + std::string(e.what()));
+
+        return 1;
+    }
 
     return 0;
 }
