601da6b1991b0049c0f846fb314c0c22c2ea645e
William Chen <exanite78012@gmail.com>
2025-02-23 01:12:11 -0500

    Cleanup program class

--- projects/VoxelRenderer/src/Program.h
+++ projects/VoxelRenderer/src/Program.h
@@ -12,6 +12,7 @@
 #include <string>
 
 #include <src/input/InputManager.h>
+#include <src/utilities/NonCopyable.h>
 #include <src/windowing/Window.h>
 
 // WASD Space Shift = movement
@@ -21,8 +22,20 @@
 // t = change between types of noise
 // scroll = change move speed
 // CTRL + scroll = change noise fill
-class Program
+class Program : public NonCopyable
 {
+private:
+    static void onOpenGlDebugMessage(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam);
+
+    static void checkForContentFolder();
+
+    // Ran first thing when the class is constructed
+    static void runEarlyStartupTests();
+
+    // Ran first thing when the run() method is called
+    // Use this if things like OpenGL need to have been initialized
+    static void runLateStartupTests();
+
 public:
     std::shared_ptr<Window> window;
     std::shared_ptr<InputManager> inputManager;
@@ -32,33 +45,17 @@ public:
     float fillAmount = 0.6;
     bool remakeNoise = false;
 
-    double noiseTime = 0;
-
     GLuint raymarcherGraphicsProgram;
     GLuint makeNoiseComputeProgram;
     GLuint makeMipMapComputeProgram;
     GLuint assignMaterialComputeProgram;
 
     Program();
-    Program(const Program&) = delete;
-    Program& operator=(const Program&) = delete;
-
     ~Program();
 
     void run();
 
     static void log(const std::string& value = "");
 
-    static void checkForContentFolder();
-
     static void assertIsTrue(bool condition, const std::string& errorMessage);
-
-    // Ran first thing when the class is constructed
-    static void runEarlyStartupTests();
-
-    // Ran first thing when the run() method is called
-    // Use this if things like OpenGL need to have been initialized
-    static void runLateStartupTests();
-
-    static void onOpenGlDebugMessage(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam);
 };
