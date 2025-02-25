215003a8ec8a19dac08f2a62ca834ca58256f12c
buildmine10 <collin_stebbins@yahoo.com>
2025-02-25 15:40:41 -0500

    Made the near clipping plane closer to the camera

--- projects/VoxelRenderer/src/Program.cpp
+++ projects/VoxelRenderer/src/Program.cpp
@@ -515,6 +515,7 @@ void Program::run()
                 reprojection.setSize(renderResolution);
                 glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
                 glDepthFunc(GL_GREATER);
+                
                 reprojection.render(*camera);
                 ImGui::Render();
                 ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
