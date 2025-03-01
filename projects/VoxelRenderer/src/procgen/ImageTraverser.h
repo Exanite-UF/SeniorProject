1a4b9888e922fd63636df247199d5dfaefd61313
RoboZero <68201467+RoboZero@users.noreply.github.com>
2025-03-01 05:39:58 -0500

    Add ImageTraverser comments

---
+++ projects/VoxelRenderer/src/procgen/ImageTraverser.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// Given image, start, and end, return best path image
+// Can lead to interesting images
+// Can be used to guarantee there is a path for player
+// Intended use was to map graph nodes as start and end, make noisy path, then convert to spline (See PoE2 talk minute 8:20)
+// A* implementation seems intuitive for single destination
+class ImageTraverser
+{
+    
+};
