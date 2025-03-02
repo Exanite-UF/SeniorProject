0eb11f04099bd601c7524ec67ee1dc0bcb46751d
William Chen <exanite78012@gmail.com>
2025-03-01 21:51:54 -0500

    Add new constants for material palettes and add documentation to all constants

--- projects/VoxelRenderer/src/Constants.h
+++ projects/VoxelRenderer/src/Constants.h
@@ -10,11 +10,35 @@ public:
     class VoxelWorld
     {
     public:
+        // This is equivalent to 2*4*4.
+        // The more occupancy mip levels we have, the higher this limit is.
+        // 2 is from storing voxels in 2x2x2 cells.
+        // 4 is from dividing by 4 twice to generate our 3 material mip levels.
         static constexpr uint32_t minSizePerAxis = 32;
+
+        // The max number of mip levels in our occupancy map.
+        // This is arbitrary, but 10 is very large amount.
+        // 10 means that the max chunk size is 2^10 = 4096.
         static constexpr uint8_t maxOccupancyMapLayerCount = 10; // 1 base layer + 9 mipmap layers
+
+        // The max number of mip levels in our material map.
         static constexpr uint8_t materialMapLayerCount = 3;
 
-        static constexpr uint32_t materialMapCount = 4096;
+        // The max number of material IDs. Material IDs are first mapped to indices representing material instances.
+        // This is equivalent to 2^12 or 2^(3*4). This is because we have 4 bits per material mip level, and we have 3 mip levels.
+        static constexpr uint32_t mippedMaterialIdCount = 4096;
+
+        // This is equivalent to 2^8.
+        static constexpr uint32_t mippedMaterialPalette0IdCount = 256;
+
+        // This is equivalent to 2^4.
+        static constexpr uint32_t mippedMaterialPalette1IdCount = 16;
+
+        // The max number of material instances.
         static constexpr uint32_t materialCount = 512;
+
+        // The max number of materials or palettes per material palette region.
+        // This is equivalent to 2^4. This is because we have 4 bits per material mip level.
+        static constexpr uint32_t materialsPerRegion = 16;
     };
 };
