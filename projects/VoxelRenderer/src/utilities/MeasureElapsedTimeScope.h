7c8bd19a7588f33968397d03e34649fe9071bca8
Exanite <42710136+Exanite@users.noreply.github.com>
2025-03-04 13:00:02 +0000

    Format code for main

--- projects/VoxelRenderer/src/utilities/MeasureElapsedTimeScope.h
+++ projects/VoxelRenderer/src/utilities/MeasureElapsedTimeScope.h
@@ -6,7 +6,7 @@ class MeasureElapsedTimeScope
 private:
     std::string name;
     std::chrono::high_resolution_clock::time_point start;
- 
+
 public:
     MeasureElapsedTimeScope(const std::string& name);
     ~MeasureElapsedTimeScope();
