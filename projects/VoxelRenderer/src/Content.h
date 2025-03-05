94c41e187e49b801699b6b1e6e014d1569c60523
buildmine10 <collin_stebbins@yahoo.com>
2025-03-05 14:22:03 -0500

    Organized voxel manipulation shaders

--- projects/VoxelRenderer/src/Content.h
+++ projects/VoxelRenderer/src/Content.h
@@ -5,22 +5,20 @@
 class Content
 {
 public:
-    constexpr static std::string_view assignMaterialComputeShader = "content/AssignMaterial.compute.glsl";
+    
     constexpr static std::string_view blitFragmentShader = "content/Blit.fragment.glsl";
     constexpr static std::string_view blitFramebufferFragmentShader = "content/BlitFramebuffer.fragment.glsl";
 
     
-    constexpr static std::string_view makeMipMapComputeShader = "content/MakeMipMap.compute.glsl";
-    constexpr static std::string_view makeNoiseComputeShader = "content/MakeNoise.compute.glsl";
+    
     constexpr static std::string_view screenTriVertexShader = "content/ScreenTri.vertex.glsl";
     
     
     
-
     constexpr static std::string_view defaultColorTexture = "content/DefaultColor.png";
     constexpr static std::string_view defaultNormalTexture = "content/DefaultNormal.png";
 
-
+    //Path tracing
     constexpr static std::string_view resetHitInfoComputeShader = "content/PathTrace/ResetHitInfo.compute.glsl";
     constexpr static std::string_view resetVisualInfoComputeShader = "content/PathTrace/ResetVisualInfo.compute.glsl";
     constexpr static std::string_view prepareRayTraceFromCameraComputeShader = "content/PathTrace/PrepareRayTraceFromCamera.compute.glsl";
@@ -28,9 +26,14 @@ public:
     constexpr static std::string_view pathTraceToFramebufferShader = "content/PathTrace/ToFramebuffer.fragment.glsl";
 
 
-
+    //Reprojection
     constexpr static std::string_view renderReprojectionVertexShader = "content/Reprojection/ReprojectionRender.vertex.glsl";
     constexpr static std::string_view renderReprojectionFragmentShader = "content/Reprojection/ReprojectionRender.fragment.glsl";
     constexpr static std::string_view combineReprojectionFragmentShader = "content/Reprojection/CombineFrames.fragment.glsl";
     constexpr static std::string_view makeCombineMaskFragmentShader = "content/Reprojection/MakeCombineMask.fragment.glsl";
+
+    //Voxel Manipulation
+    constexpr static std::string_view makeMipMapComputeShader = "content/VoxelManipulation/MakeMipMap.compute.glsl";
+    constexpr static std::string_view makeNoiseComputeShader = "content/VoxelManipulation/MakeNoise.compute.glsl";
+    constexpr static std::string_view assignMaterialComputeShader = "content/VoxelManipulation/AssignMaterial.compute.glsl";
 };
