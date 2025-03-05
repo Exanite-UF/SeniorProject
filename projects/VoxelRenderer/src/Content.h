5252bd8b457951cd3fcedcf798272d83ea0bdfc8
buildmine10 <collin_stebbins@yahoo.com>
2025-03-05 14:20:26 -0500

    Organized Reprojection Shaders

--- projects/VoxelRenderer/src/Content.h
+++ projects/VoxelRenderer/src/Content.h
@@ -14,10 +14,8 @@ public:
     constexpr static std::string_view makeNoiseComputeShader = "content/MakeNoise.compute.glsl";
     constexpr static std::string_view screenTriVertexShader = "content/ScreenTri.vertex.glsl";
     
-    constexpr static std::string_view renderReprojectionVertexShader = "content/ReprojectionRender.vertex.glsl";
-    constexpr static std::string_view renderReprojectionFragmentShader = "content/ReprojectionRender.fragment.glsl";
-    constexpr static std::string_view combineReprojectionFragmentShader = "content/CombineFrames.fragment.glsl";
-    constexpr static std::string_view makeCombineMaskFragmentShader = "content/MakeCombineMask.fragment.glsl";
+    
+    
 
     constexpr static std::string_view defaultColorTexture = "content/DefaultColor.png";
     constexpr static std::string_view defaultNormalTexture = "content/DefaultNormal.png";
@@ -28,4 +26,11 @@ public:
     constexpr static std::string_view prepareRayTraceFromCameraComputeShader = "content/PathTrace/PrepareRayTraceFromCamera.compute.glsl";
     constexpr static std::string_view fullCastComputeShader = "content/PathTrace/FullCast.compute.glsl";
     constexpr static std::string_view pathTraceToFramebufferShader = "content/PathTrace/ToFramebuffer.fragment.glsl";
+
+
+
+    constexpr static std::string_view renderReprojectionVertexShader = "content/Reprojection/ReprojectionRender.vertex.glsl";
+    constexpr static std::string_view renderReprojectionFragmentShader = "content/Reprojection/ReprojectionRender.fragment.glsl";
+    constexpr static std::string_view combineReprojectionFragmentShader = "content/Reprojection/CombineFrames.fragment.glsl";
+    constexpr static std::string_view makeCombineMaskFragmentShader = "content/Reprojection/MakeCombineMask.fragment.glsl";
 };
