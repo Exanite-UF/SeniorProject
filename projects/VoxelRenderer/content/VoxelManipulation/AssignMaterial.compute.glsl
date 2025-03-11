eb195a520419213cea33f09b3286f5ff33b1ba51
William Chen <exanite78012@gmail.com>
2025-03-11 19:22:54 -0400

    Rewrite both ExaniteWorldGenerator and AssignMaterial shader

--- projects/VoxelRenderer/content/VoxelManipulation/AssignMaterial.compute.glsl
+++ projects/VoxelRenderer/content/VoxelManipulation/AssignMaterial.compute.glsl
@@ -28,6 +28,25 @@ void setMaterialIndex(ivec3 voxelPosition, uint materialIndex)
     materialMap[i32Index] |= materialIndex << bitsShifted;
 }
 
+uint sampleMaterialIndex(ivec3 voxelPosition)
+{
+    ivec3 size2 = voxelCount >> 4;
+
+    ivec3 position0 = voxelPosition;
+    ivec3 position1 = position0 >> 2;
+    ivec3 position2 = position0 >> 4;
+
+    uint materialBits0 = ((position0.z & 1) << 2) | ((position0.y & 1) << 1) | ((position0.x & 1) << 0);
+    uint materialBits1 = ((position1.z & 1) << 2) | ((position1.y & 1) << 1) | ((position1.x & 1) << 0);
+    uint materialBits2 = ((position2.z & 1) << 2) | ((position2.y & 1) << 1) | ((position2.x & 1) << 0);
+
+    uint materialOffset = position2.x + size2.x * (position2.y + size2.y * position2.z);
+
+    uint materialIndex = ((materialBits2 << 6) | (materialBits1 << 3) | (materialBits0 << 0)) + materialOffset;
+
+    return materialIndex % 65536;
+}
+
 void main()
 {
     ivec3 voxelPosition = ivec3(gl_GlobalInvocationID.xyz);
@@ -36,5 +55,6 @@ void main()
     int i16Index = (voxelPosition.x + voxelCount.x * (voxelPosition.y + voxelCount.y * voxelPosition.z));
     int i32Index = i16Index / 2;
 
-    materialMap[i32Index] = (1 << 16) | (2 << 0); // TODO: Create a more interesting pattern
+    setMaterialIndex(ivec3(voxelPosition.x, voxelPosition.y, voxelPosition.z), sampleMaterialIndex(ivec3(voxelPosition.x, voxelPosition.y, voxelPosition.z)));
+    setMaterialIndex(ivec3(voxelPosition.x + 1, voxelPosition.y, voxelPosition.z), sampleMaterialIndex(ivec3(voxelPosition.x + 1, voxelPosition.y, voxelPosition.z)));
 }
