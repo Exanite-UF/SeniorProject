473d4e31d68ad66d59e9a5795407591c38a28700
William Chen <exanite78012@gmail.com>
2025-03-12 23:22:11 -0400

    Revert "Remove Transform class"
    This reverts commit e45929a70c0a7c20a3278016e6226d47e6e3606e.

---
+++ projects/VoxelRenderer/src/world/Transform.cpp
@@ -0,0 +1,127 @@
+#include "Transform.h"
+
+const glm::vec3& Transform::getLocalPosition() const
+{
+    return position;
+}
+
+const glm::quat& Transform::getLocalRotation() const
+{
+    return rotation;
+}
+
+const glm::vec3& Transform::getLocalScale() const
+{
+    return scale;
+}
+
+glm::vec3 Transform::getRightDirection() const
+{
+    return getGlobalRotation() * glm::vec3(0, -1, 0);
+}
+
+glm::vec3 Transform::getUpDirection() const
+{
+    return getGlobalRotation() * glm::vec3(0, 0, 1);
+}
+
+glm::vec3 Transform::getForwardDirection() const
+{
+    return getGlobalRotation() * glm::vec3(1, 0, 0);
+}
+
+// TODO: This currently does the same as the local version. Needs a proper implementation.
+glm::vec3 Transform::getGlobalPosition() const
+{
+    return position;
+}
+
+// TODO: This currently does the same as the local version. Needs a proper implementation.
+glm::quat Transform::getGlobalRotation() const
+{
+    return rotation;
+}
+
+// TODO: This currently does the same as the local version. Needs a proper implementation.
+glm::vec3 Transform::getGlobalScale() const
+{
+    return scale;
+}
+
+void Transform::setLocalPosition(const glm::vec3& value)
+{
+    position = value;
+    // TODO: Propagate transform changes to children or mark self as dirty
+}
+
+void Transform::addLocalPosition(const glm::vec3& value)
+{
+    position += value;
+    // TODO: Propagate transform changes to children or mark self as dirty
+}
+
+void Transform::setLocalRotation(const glm::quat& value)
+{
+    rotation = value;
+    // TODO: Propagate transform changes to children or mark self as dirty
+}
+
+void Transform::addLocalRotation(const glm::quat& value)
+{
+    rotation = value * rotation;
+    // TODO: Propagate transform changes to children or mark self as dirty
+}
+
+void Transform::setLocalScale(const glm::vec3& value)
+{
+    scale = value;
+    // TODO: Propagate transform changes to children or mark self as dirty
+}
+
+void Transform::multiplyLocalScale(const glm::vec3& value)
+{
+    scale *= value;
+    // TODO: Propagate transform changes to children or mark self as dirty
+}
+
+// TODO: This currently does the same as the local version. Needs a proper implementation.
+void Transform::setGlobalPosition(const glm::vec3& value)
+{
+    position = value;
+    // TODO: Propagate transform changes to children or mark self as dirty
+}
+
+// TODO: This currently does the same as the local version. Needs a proper implementation.
+void Transform::addGlobalPosition(const glm::vec3& value)
+{
+    position += value;
+    // TODO: Propagate transform changes to children or mark self as dirty
+}
+
+// TODO: This currently does the same as the local version. Needs a proper implementation.
+void Transform::setGlobalRotation(const glm::quat& value)
+{
+    rotation = value;
+    // TODO: Propagate transform changes to children or mark self as dirty
+}
+
+// TODO: This currently does the same as the local version. Needs a proper implementation.
+void Transform::addGlobalRotation(const glm::quat& value)
+{
+    rotation = value * rotation;
+    // TODO: Propagate transform changes to children or mark self as dirty
+}
+
+// TODO: This currently does the same as the local version. Needs a proper implementation.
+void Transform::setGlobalScale(const glm::vec3& value)
+{
+    scale = value;
+    // TODO: Propagate transform changes to children or mark self as dirty
+}
+
+// TODO: This currently does the same as the local version. Needs a proper implementation.
+void Transform::multiplyGlobalScale(const glm::vec3& value)
+{
+    scale *= value;
+    // TODO: Propagate transform changes to children or mark self as dirty
+}
