119a62dc8d5a57e6396d840f1876e4debeac2262
RoboZero <68201467+RoboZero@users.noreply.github.com>
2025-03-20 02:29:55 -0400

    Add stone terrain parameters to prototype world

--- projects/VoxelRenderer/src/procgen/generators/PrototypeWorldGenerator.cpp
+++ projects/VoxelRenderer/src/procgen/generators/PrototypeWorldGenerator.cpp
@@ -1,22 +1,24 @@
 #include <src/procgen/generators/PrototypeWorldGenerator.h>
-#include <src/procgen/synthesizers/TextureOctaveNoiseSynthesizer.h>
 #include <imgui/imgui.h>
 #include <memory>
 #include <src/procgen/PrintUtility.h>
 #include <src/world/VoxelWorldData.h>
 
+#include <src/procgen/synthesizers/TextureOctaveNoiseSynthesizer.h>
+
 void PrototypeWorldGenerator::generateData()
 {
     glm::ivec3 size = { data.getSize().x, data.getSize().y, 1 };
-    this->textureData = std::make_shared<TextureData>(size);
-    textureDataSynthesizer->generate(textureData);
+
+    siv::BasicPerlinNoise<float> perlinNoise(seed);
 
     for (int x = 0; x < data.getSize().x; ++x)
     {
         for (int y = 0; y < data.getSize().y; ++y)
         {
-            float noise = textureData->get(x, y);
-            int offset = (int)(baseHeight + (noise * data.getSize().z));
+            // Stone Terrain
+            float perlinNoiseSample = perlinNoise.octave2D_01(x * frequency, y * frequency, octaves, persistence);
+            int offset = (int)(baseHeight + (perlinNoiseSample * terrainMaxAmplitude));
             int height = glm::min(data.getSize().z, offset);
 
             for (int z = 0; z < height; ++z)
@@ -29,12 +31,21 @@ void PrototypeWorldGenerator::generateData()
 
 void PrototypeWorldGenerator::showDebugMenu()
 {
-    // TODO: Testing. Once finalized, add to existing Imgui fields.
+    // TODO: Testing. Once finalized, add to existing Imgwui fields.
     ImGui::PushID("PrototypeWorldGenerator");
     {
-        if (ImGui::CollapsingHeader("Texture-Heightmap World Generator (F8)"))
+        if (ImGui::CollapsingHeader("Prototype World Generator (F8)"))
         {
-            ImGui::SliderFloat("Base Height", &baseHeight, 0, data.getSize().z);
+            ImGui::SliderInt("Base Height", &baseHeight, 0, data.getSize().z);
+            ImGui::SliderInt("Octaves", &octaves, 1, 5);
+            ImGui::SliderFloat("Persistence", &persistence, 0, 1);
+            ImGui::SliderFloat("Frequency", &frequency, 0, 1);
+            
+    int octaves = 3;
+    float persistence = 0.5; 
+    int baseHeight = 100;
+    float frequency = 10;
+    int terrainMaxAmplitude = 100;
             textureDataSynthesizer->showDebugMenu();
             if (ImGui::Button("Print Texture"))
             {
