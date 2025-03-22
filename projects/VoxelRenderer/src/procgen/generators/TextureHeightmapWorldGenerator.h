f5c8657d3f53dbb4725b3119c1fe26f19c4863df
William Chen <exanite78012@gmail.com>
2025-03-22 03:18:37 -0400

    Add chunkSize and chunkPosition properties to WorldGenerator class

--- projects/VoxelRenderer/src/procgen/generators/TextureHeightmapWorldGenerator.h
+++ projects/VoxelRenderer/src/procgen/generators/TextureHeightmapWorldGenerator.h
@@ -9,12 +9,11 @@ private:
     std::shared_ptr<TextureDataSynthesizer> textureDataSynthesizer;
     std::shared_ptr<TextureData> textureData;
     float baseHeight = 100;
-    glm::ivec3 chunkSize;
 
     void generateData(VoxelChunkData& data) override;
 
 public:
-    explicit TextureHeightmapWorldGenerator(const glm::ivec3& chunkSize, const std::shared_ptr<TextureDataSynthesizer>& textureDataSynthesizer);
+    explicit TextureHeightmapWorldGenerator(const std::shared_ptr<TextureDataSynthesizer>& textureDataSynthesizer);
 
     void showDebugMenu() override;
 };
