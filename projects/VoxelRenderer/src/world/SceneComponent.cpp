dcd380f415218b177f659b681d14ed341e9f06ac
William Chen <exanite78012@gmail.com>
2025-03-22 04:00:49 -0400

    Refactor SceneComponent class and prepare for chunk displaying

--- projects/VoxelRenderer/src/world/SceneComponent.cpp
+++ projects/VoxelRenderer/src/world/SceneComponent.cpp
@@ -3,6 +3,29 @@
 #define GLM_ENABLE_EXPERIMENTAL
 #include "glm/gtx/quaternion.hpp"
 
+void SceneComponent::setCamera(const std::shared_ptr<CameraComponent>& camera)
+{
+    this->camera = camera;
+}
+
+const std::shared_ptr<CameraComponent>& SceneComponent::getCamera()
+{
+    return camera;
+}
+
+const std::vector<std::shared_ptr<VoxelChunkComponent>>& SceneComponent::getChunks()
+{
+    return chunks;
+}
+
+bool SceneComponent::tryGetChunkAtPosition(const glm::ivec3& chunkPosition, std::shared_ptr<VoxelChunkComponent>& result)
+{
+    auto iterator = chunksByChunkPosition.find(chunkPosition);
+    result = iterator->second;
+
+    return iterator != chunksByChunkPosition.end();
+}
+
 bool SceneComponent::tryGetClosestChunk(std::shared_ptr<VoxelChunkComponent>& result)
 {
     auto cameraPosition = camera->getTransform()->getGlobalPosition();
@@ -22,4 +45,30 @@ bool SceneComponent::tryGetClosestChunk(std::shared_ptr<VoxelChunkComponent>& re
     }
 
     return !!result;
+}
+
+void SceneComponent::addChunk(const glm::ivec3& chunkPosition, std::shared_ptr<VoxelChunkComponent>& chunk)
+{
+    if (chunksByChunkPosition.emplace(chunkPosition, chunk).second)
+    {
+        // If emplace succeeds, also add it to the flattened vector
+        chunks.push_back(chunk);
+    }
+}
+
+void SceneComponent::removeChunk(const glm::ivec3& chunkPosition, const std::shared_ptr<VoxelChunkComponent>& chunk)
+{
+    // Find chunk in chunksByChunkPosition and erase it
+    auto mapIterator = chunksByChunkPosition.find(chunkPosition);
+    if (mapIterator != chunksByChunkPosition.end() && mapIterator->second == chunk)
+    {
+        chunksByChunkPosition.erase(mapIterator);
+
+        // If removal from chunksByChunkPosition succeeds, also remove it from the flattened vector
+        auto vectorIterator = std::find(chunks.begin(), chunks.end(), chunk);
+        if (vectorIterator != chunks.end())
+        {
+            chunks.erase(vectorIterator);
+        }
+    }
 }
