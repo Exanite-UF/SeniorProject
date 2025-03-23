6c1ee7f271e16fdc4c4c6ef582939e164d987e5e
William Chen <exanite78012@gmail.com>
2025-03-23 09:10:17 -0400

    Design VoxelChunkCommandBuffer API

--- projects/VoxelRenderer/src/world/VoxelChunkCommandBuffer.h
+++ projects/VoxelRenderer/src/world/VoxelChunkCommandBuffer.h
@@ -1,6 +1,23 @@
 #pragma once
 
-// A command buffer for commands related to modifying a VoxelChunk
+#include <src/world/VoxelChunkComponent.h>
+
+// A command buffer for commands related to modifying a VoxelChunkComponent
 class VoxelChunkCommandBuffer
 {
+public:
+    void setSize(const glm::ivec3& size);
+
+    void setVoxelOccupancy(const glm::ivec3& position, bool isOccupied);
+    void setVoxelMaterial(const glm::ivec3& position, const std::shared_ptr<Material>& material);
+    void setVoxelMaterialIndex(const glm::ivec3& position, uint16_t materialIndex);
+
+    void clearOccupancyMap();
+    void clearMaterialMap();
+
+    void copyFrom(VoxelChunkData& data);
+
+public:
+    void apply(const std::shared_ptr<VoxelChunkComponent>& chunk);
+    void clear();
 };
