3b94e31e05dc2ff50bc19ec04d2981cde9d67b01
Gio <44237862+Redifexx@users.noreply.github.com>
2025-03-22 22:52:43 -0400

    Added shaders for triagulation

---
+++ projects/VoxelRenderer/content/Triangulation/Phong.vertex.glsl
@@ -0,0 +1,22 @@
+#version 330 core
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec2 aTexCoord;
+layout (location = 2) in vec3 aNormal;
+  
+out vec3 FragPos;
+out vec3 Normal;
+out vec2 TexCoord;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+void main()
+{
+    gl_Position = projection * view * model * vec4(aPos, 1.0);
+    FragPos = vec3(model * vec4(aPos, 1.0f));
+    Normal = mat3(transpose(inverse(model))) * aNormal; //ineficient but good for now
+    gl_Position.z = 2.0 * log(gl_Position.w/0.001)/log(1000/0.001) - 1;
+    gl_Position.z *= gl_Position.w;
+    TexCoord = aTexCoord;
+}    
