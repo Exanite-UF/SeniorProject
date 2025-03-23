51174fa3edad736d4e6e9908e350876637d30925
Gio <44237862+Redifexx@users.noreply.github.com>
2025-03-22 21:36:52 -0400

    mesh and shader class

---
+++ projects/VoxelRenderer/src/voxelizer/mesh.h
@@ -0,0 +1,53 @@
+#ifndef MESH_H
+#define MESH_H
+
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <GLM/glm.hpp>
+#include <GLM/gtc/matrix_transform.hpp>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <glm/gtc/type_ptr.hpp>
+#include <cmath>
+#include <chrono>
+#include <src/voxelizer/shader.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+struct Vertex
+{
+    glm::vec3 Position;
+    glm::vec3 Normal;
+    glm::vec3 TexCoords;
+    glm::vec3 Tangent;
+    glm::vec3 Bitangent;
+};
+
+// Not sure if we'll end up using this
+struct Texture
+{
+    unsigned int id;
+    std::string type;
+    std::string path;
+};
+
+class Mesh
+{
+private:
+    unsigned int VAO, VBO, EBO;
+    void setupMesh();
+public:
+    std::vector<Vertex> vertices;
+    std::vector<unsigned int> indices;
+    std::vector<Texture> textures;
+
+    Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
+    void Draw(Shader &shader);
+};
+
+
+#endif
