4c87ff711d75a03199896c4abe4115af094bcc6f
William Chen <exanite78012@gmail.com>
2025-03-24 07:41:40 -0400

    Add log timestamps

--- projects/VoxelRenderer/src/utilities/Log.cpp
+++ projects/VoxelRenderer/src/utilities/Log.cpp
@@ -1,44 +1,101 @@
 #include "Log.h"
 
+#include <chrono>
+#include <iomanip>
 #include <iostream>
+#include <sstream>
 
-void Log::verbose(const std::string& value)
+std::string Log::getCurrentTimeText()
 {
-    write(Verbose, value);
+    // Get the current time
+    auto now = std::chrono::system_clock::now();
+    auto in_time_t = std::chrono::system_clock::to_time_t(now);
+
+    // Convert to a tm structure
+    std::tm buf;
+    localtime_r(&in_time_t, &buf);
+
+    // Format the time as HH:MM:SS
+    std::ostringstream oss;
+    oss << std::put_time(&buf, "%H:%M:%S");
+    return oss.str();
 }
 
-void Log::debug(const std::string& value)
+void Log::verbose(const std::string& message)
 {
-    write(Debug, value);
+    write(Verbose, message);
 }
 
-void Log::information(const std::string& value)
+void Log::debug(const std::string& message)
 {
-    write(Information, value);
+    write(Debug, message);
 }
 
-void Log::warning(const std::string& value)
+void Log::information(const std::string& message)
 {
-    write(Warning, value);
+    write(Information, message);
 }
 
-void Log::error(const std::string& value)
+void Log::warning(const std::string& message)
 {
-    write(Error, value);
+    write(Warning, message);
 }
 
-void Log::fatal(const std::string& value)
+void Log::error(const std::string& message)
 {
-    write(Fatal, value);
+    write(Error, message);
 }
 
-void Log::write(const LogLevel level, const std::string& value)
+void Log::fatal(const std::string& message)
+{
+    write(Fatal, message);
+}
+
+std::string Log::getLogLevelText(const Log::LogLevel level)
+{
+    std::string logLevelText;
+    switch (level)
+    {
+        case Verbose:
+        {
+            return "VRB";
+        }
+        case Debug:
+        {
+            return "DBG";
+        }
+        case Information:
+        {
+            return "INF";
+        }
+        case Warning:
+        {
+            return "WRN";
+        }
+        case Error:
+        {
+            return "ERR";
+        }
+        case Fatal:
+        {
+            return "FTL";
+        }
+        default:
+        {
+            throw std::runtime_error("Unknown log level");
+        }
+    }
+}
+
+void Log::write(const LogLevel level, const std::string& message)
 {
     if (level < minimumLevel)
     {
         return;
     }
 
-    std::cout << value + "\n"
-              << std::flush;
+    std::string timeText = getCurrentTimeText();
+    std::string levelText = getLogLevelText(level);
+
+    std::cout << std::format("[{} {}] {}\n", timeText, levelText, message) << std::flush;
 }
