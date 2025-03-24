1b2e68eb88438f13aab86865e1cde6fb00bce5c1
William Chen <exanite78012@gmail.com>
2025-03-24 07:30:34 -0400

    Add log levels

--- projects/VoxelRenderer/src/utilities/Log.cpp
+++ projects/VoxelRenderer/src/utilities/Log.cpp
@@ -2,8 +2,43 @@
 
 #include <iostream>
 
+void Log::verbose(const std::string& value)
+{
+    write(Verbose, value);
+}
+
+void Log::debug(const std::string& value)
+{
+    write(Debug, value);
+}
+
 void Log::information(const std::string& value)
 {
+    write(Information, value);
+}
+
+void Log::warning(const std::string& value)
+{
+    write(Warning, value);
+}
+
+void Log::error(const std::string& value)
+{
+    write(Error, value);
+}
+
+void Log::fatal(const std::string& value)
+{
+    write(Fatal, value);
+}
+
+void Log::write(const LogLevel level, const std::string& value)
+{
+    if (level < minimumLevel)
+    {
+        return;
+    }
+
     std::cout << value + "\n"
               << std::flush;
 }
--- projects/VoxelRenderer/src/utilities/Log.h
+++ projects/VoxelRenderer/src/utilities/Log.h
@@ -1,9 +1,29 @@
 #pragma once
 
+#include <atomic>
 #include <string>
 
 class Log
 {
+    enum LogLevel
+    {
+        Verbose = 0,
+        Debug = 1,
+        Information = 2,
+        Warning = 3,
+        Error = 4,
+        Fatal = 5,
+    };
+
 public:
-    static void log(const std::string& value = "");
+    inline static std::atomic<LogLevel> minimumLevel = Information;
+
+    static void verbose(const std::string& value);
+    static void debug(const std::string& value);
+    static void information(const std::string& value);
+    static void warning(const std::string& value);
+    static void error(const std::string& value);
+    static void fatal(const std::string& value);
+
+    static void write(LogLevel level, const std::string& value);
 };
