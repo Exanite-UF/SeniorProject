50f956887991d2dfd1374cf4c0610610a2be4354
RoboZero <68201467+RoboZero@users.noreply.github.com>
2025-03-25 02:40:31 -0400

    Add copy functionality to FlatArrayData

--- projects/VoxelRenderer/src/procgen/data/FlatArrayData.h
+++ projects/VoxelRenderer/src/procgen/data/FlatArrayData.h
@@ -15,6 +15,7 @@ private:
 
 public:
     FlatArrayData(glm::ivec3 inSize);
+    bool copyList(std::initializer_list<T> list);
     T get(int x, int y, int z = 0);
     void set(T value, int x, int y, int z = 0);
 
@@ -26,6 +27,18 @@ FlatArrayData<T>::FlatArrayData(glm::ivec3 inSize)
 {
     this->size = inSize;
     this->map.resize(size.x * size.y * size.z, 0);
+}
+
+template <typename T>
+bool FlatArrayData<T>::copyList(std::initializer_list<T> list)
+{
+    if(list.size() != size.x * size.y * size.z)
+    {
+        return false;
+    }
+
+    map = list;
+    return true;
 }
 
 template <typename T>
