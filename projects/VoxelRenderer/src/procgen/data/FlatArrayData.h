954c2249eb84f1eb1fd20a9b401ec8036d1ee945
RoboZero <68201467+RoboZero@users.noreply.github.com>
2025-03-25 01:59:24 -0400

    Add FlatArrayData base class for world generator use

--- projects/VoxelRenderer/src/procgen/data/FlatArrayData.h
+++ projects/VoxelRenderer/src/procgen/data/FlatArrayData.h
@@ -7,16 +7,42 @@
 // 2D or 3D array of single values
 // AKA Texture but that's already used.
 template <typename T> 
-class TextureData
+class FlatArrayData
 {
 private:
     glm::ivec3 size;
     std::vector<T> map;
 
 public:
-    TextureData(glm::ivec3 inSize);
+    FlatArrayData(glm::ivec3 inSize);
     T get(int x, int y, int z = 0);
     void set(T value, int x, int y, int z = 0);
 
     const glm::ivec3& getSize();
 };
+
+template <typename T>
+FlatArrayData<T>::FlatArrayData(glm::ivec3 inSize)
+{
+    this->size = inSize;
+    this->map.resize(size.x * size.y * size.z, 0);
+}
+
+template <typename T>
+T FlatArrayData<T>::get(int x, int y, int z)
+{
+    return this->map[x + y * size.x + z * size.y * size.x];
+}
+
+template <typename T>
+void FlatArrayData<T>::set(T value, int x, int y, int z)
+{
+    this->map[x + y * size.x + z * size.y * size.x] = value;
+}
+
+template <typename T>
+const glm::ivec3& FlatArrayData<T>::getSize()
+{
+    return size;
+}
+
---
+++ projects/VoxelRenderer/src/procgen/data/TextureData.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <src/procgen/data/FlatArrayData.h>
+
+class TextureData : public FlatArrayData<float>{ };
