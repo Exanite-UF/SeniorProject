1217eb3525ddf18b7343ca7c2e125334e0c108a1
William Chen <exanite78012@gmail.com>
2025-04-04 18:49:57 -0400

    Update shader versions

--- projects/VoxelRenderer/content/Triangulation/Voxel.vertex.glsl
+++ projects/VoxelRenderer/content/Triangulation/Voxel.vertex.glsl
@@ -1,4 +1,5 @@
-#version 330 core
+#version 460 core
+
 layout(location = 0) in vec3 aPos; // Cube vertex positions
 layout(location = 1) in vec3 instancePos; // Instance position for each voxel
 layout(location = 2) in vec2 aTexCoord;
