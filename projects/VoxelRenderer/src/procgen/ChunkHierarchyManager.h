0d85beac051758d2b7a64be71061580b4142d7b8
William Chen <exanite78012@gmail.com>
2025-04-05 02:56:41 -0400

    Implement initial ChunkHierarchyManager

--- projects/VoxelRenderer/src/procgen/ChunkHierarchyManager.h
+++ projects/VoxelRenderer/src/procgen/ChunkHierarchyManager.h
@@ -1,6 +1,11 @@
 #pragma once
 
+#include <glm/glm.hpp>
 #include <src/utilities/Singleton.h>
+
+class StructureNode
+{
+};
 
 class ChunkHierarchyManager : public Singleton<ChunkHierarchyManager>
 {
@@ -12,10 +17,35 @@ class ChunkHierarchyManager : public Singleton<ChunkHierarchyManager>
     // Chunk1 position: 5   / 2   -> 2.5  | floor(2.5), ceil(2.5) -> 2, [3]
     // Chunk2 position: 3   / 2   -> 1.5  | floor(1.5), ceil(1.5) -> 1, 2
 
-    // std::unordered_map<int, std::vector<Structure>> chunk0;
-    // std::unordered_map<int, std::vector<Structure>> chunk1;
-    // std::unordered_map<int, std::vector<Structure>> chunk2;
+private:
+    std::vector<std::unordered_map<int, std::vector<StructureNode>>> chunks;
 
-    // std::vector<std::unordered_map<int, std::vector<Structure>>> chunks;
-    // std::array<std::unordered_map<int, std::vector<Structure>>, 3> chunks;
+public:
+    std::vector<StructureNode> getStructuresForChunkPosition(int chunkPosition)
+    {
+        std::vector<StructureNode> results {};
+        float chunkPositionF = chunkPosition;
+
+        for (int i = 0; i < chunks.size(); i++)
+        {
+            int first = glm::floor(chunkPositionF / 2);
+            int second = glm::ceil(chunkPositionF / 2);
+
+            auto firstIterator = chunks.at(i).find(first);
+            if (firstIterator != chunks.at(i).end())
+            {
+                results.insert(results.end(), firstIterator->second.begin(), firstIterator->second.end());
+            }
+
+            auto secondIterator = chunks.at(i).find(second);
+            if (secondIterator != chunks.at(i).end())
+            {
+                results.insert(results.end(), secondIterator->second.begin(), secondIterator->second.end());
+            }
+
+            chunkPositionF = glm::ceil(chunkPositionF / 2);
+        }
+
+        return results;
+    };
 };
