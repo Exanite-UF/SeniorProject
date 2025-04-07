159c4eec51dca7f47789e1ad859392bc5e9a435d
William Chen <exanite78012@gmail.com>
2025-04-07 18:40:40 -0400

    Fix incorrect indices being calculated

--- projects/VoxelRenderer/src/world/VoxelChunkUtility.cpp
+++ projects/VoxelRenderer/src/world/VoxelChunkUtility.cpp
@@ -14,8 +14,11 @@ std::vector<uint32_t> VoxelChunkUtility::getOccupancyMapIndices(const glm::ivec3
     Assert::isTrue((size.y != 0) && ((size.y & (size.y - 1)) == 0), "size.y must be a power of 2");
     Assert::isTrue((size.z != 0) && ((size.z & (size.z - 1)) == 0), "size.z must be a power of 2");
 
+    // The division inside the log2 call is a 4, not 2, because the mipmap generation will break if the top level mipmap has side length 2. This prevents that from occurring.
+    // The mipmap generation breaks when the side length is 2 because the top level mipmap will be less than a full uint32 in size.
+    // A minimum side length makes the top level mipmap be at least 1 uint32 in size.
     auto smallestSide = std::min(std::min(size.x, size.y), size.z);
-    int mipmapLayerCount = std::max(0, static_cast<int>(std::floor(std::log2(smallestSide / 2))));
+    int mipmapLayerCount = std::max(0, static_cast<int>(std::floor(std::log2(smallestSide / 4) / 2)));
     int layerCount = 1 + mipmapLayerCount;
     layerCount = glm::min(layerCount, static_cast<int>(Constants::VoxelChunk::maxOccupancyMapLayerCount)); // Limit the max number of mip maps
 
@@ -31,7 +34,7 @@ std::vector<uint32_t> VoxelChunkUtility::getOccupancyMapIndices(const glm::ivec3
         totalBitCount = ((totalBitCount + 8 - 1) / 8) * 8; // Round to next multiple of 8
         indices.push_back(totalBitCount / 8);
 
-        currentSize >>= 1;
+        currentSize >>= 2;
     }
 
     return indices;
