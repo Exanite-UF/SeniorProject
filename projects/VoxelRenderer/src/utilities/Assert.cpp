de6c890fdf4a06bb6430027004f097c919f532d3
William Chen <exanite78012@gmail.com>
2025-04-07 01:30:55 -0400

    Also log error message if assert fails

--- projects/VoxelRenderer/src/utilities/Assert.cpp
+++ projects/VoxelRenderer/src/utilities/Assert.cpp
@@ -1,11 +1,13 @@
 #include "Assert.h"
 
+#include <src/utilities/Log.h>
 #include <stdexcept>
 
 void Assert::isTrue(const bool condition, const std::string& errorMessage)
 {
     if (!condition)
     {
+        Log::error("Assert failed: " + errorMessage);
         throw std::runtime_error("Assert failed: " + errorMessage);
     }
 }
