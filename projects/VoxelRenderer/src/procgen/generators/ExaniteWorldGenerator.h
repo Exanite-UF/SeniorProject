9174e6b52926279b5b606df4e26d8469d537adc5
William Chen <exanite78012@gmail.com>
2025-04-16 05:47:34 -0400

    Add city generator

--- projects/VoxelRenderer/src/procgen/generators/ExaniteWorldGenerator.h
+++ projects/VoxelRenderer/src/procgen/generators/ExaniteWorldGenerator.h
@@ -7,4 +7,7 @@ class ExaniteWorldGenerator : public WorldGenerator
 public:
     void generateData(VoxelChunkData& data) override;
     void showDebugMenu() override;
+
+protected:
+    void generateBuilding(VoxelChunkData& data, const glm::ivec3& cornerPosition, const glm::ivec3& size);
 };
--- projects/VoxelRenderer/src/procgen/generators/WorldGenerator.h
+++ projects/VoxelRenderer/src/procgen/generators/WorldGenerator.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <glm/vec3.hpp>
-
 #include <memory>
 
 #include <src/Constants.h>
